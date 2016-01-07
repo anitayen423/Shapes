dc3f0e208c32a77ac0754bfe8e7107877346a51f
anitayen423 <anitayen423@gmail.com>
2016-01-07 08:00:22 -0800

    Create Prism.java

---
+++ Prism.java
@@ -0,0 +1,29 @@
+public abstract class Prism implements Shape{
+	private double height;
+	private double width;
+	private double length;
+	
+	public double getHeight() {
+		return height;
+	}
+	
+	public double getWidth() {
+		return height;
+	}
+	
+	public double getLength() {
+		return height;
+	}
+	
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	
+	public void setLength(double length) {
+		this.length = length;
+	}
+}
